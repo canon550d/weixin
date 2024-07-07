3fae2f939108dac305e90bca35a079bc9a85d213
Sya <Sya@Home>
2024-07-08 00:43:54 +0800

    Dao方法都变成了Mapper

--- source/java/src/main/java/org/hb0712/discovery/service/impl/ImageServiceImpl.java
+++ source/java/src/main/java/org/hb0712/discovery/service/impl/ImageServiceImpl.java
@@ -1,58 +1,66 @@
 package org.hb0712.discovery.service.impl;
 
 import java.io.File;
-import java.text.ParseException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.apache.commons.io.FileUtils;
-import org.hb0712.discovery.dao.ImageDao;
 import org.hb0712.discovery.dao.impl.Page;
+import org.hb0712.discovery.mapper.ImageMapper;
 import org.hb0712.discovery.pojo.Camera;
-import org.hb0712.discovery.pojo.ImageFile;
 import org.hb0712.discovery.pojo.Image;
 import org.hb0712.discovery.service.ImageService;
-import org.jboss.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImageServiceImpl extends FileServiceImpl implements ImageService{
-	private Logger logger = Logger.getLogger(ImageServiceImpl.class);
+	private Logger logger = LoggerFactory.getLogger(ImageServiceImpl.class);
 	
 	@Autowired
-	public ImageDao imageDao;
+	private FileConfig fileConfig;
+	@Autowired
+	public ImageMapper imageMapper;
 	
 	public List<Image> list(Page page){
-		return imageDao.list(page);
+		Integer total = imageMapper.getCount(null);
+		page.setTotal(total);
+		
+		return imageMapper.getImages(page, null, null);
 	}
 	
 	public List<Image> list(Page page, String orderby, Camera camera){
-		return imageDao.list(page, orderby, camera);
+		Integer total = imageMapper.getCount(camera);
+		page.setTotal(total);
+		
+		return imageMapper.getImages(page, camera, null);
 	}
 	
 	public Image getImage(String id) {
-		int int_id = Integer.valueOf(id);
-		return imageDao.getImage(int_id);
+		return imageMapper.getImageById(Integer.valueOf(id));
 	}
 	
 	public Image getImageByMd5(String md5) {
-		return imageDao.getImage(md5);
+		return imageMapper.getImageByMD5(md5);
 	}
 	
 	public boolean update(Image image) {
-		return imageDao.update(image);
+		return imageMapper.update(image);
 	}
 	
 	public boolean save(Image image) {
-		return imageDao.save(image);
+		return imageMapper.save(image);
 	}
 	
 	public List<Image> list(String[] ids){
@@ -60,22 +68,54 @@ public class ImageServiceImpl extends FileServiceImpl implements ImageService{
 		for(int i=0;i<ids.length;i++) {
 			int_ids[i] = Integer.valueOf(ids[i]);
 		}
-		return imageDao.list(int_ids);
+		return imageMapper.getImagesByIds(int_ids);
 	}
-	public List<Image> listOrderBy(String orderby){
-		return imageDao.listOrderBy(orderby);
-	}
+//	public List<Image> listOrderBy(String orderby){
+//		return imageDao.listOrderBy(orderby);
+//	}
 	
 	public List<Image> listRepeat(String camera_id){
-		return imageDao.listRepeat(camera_id);
+		List<Image> list = imageMapper.getImagesByCameraId(camera_id);
+		
+		List<Image> list2 = new ArrayList<Image>();
+		for (Image i:list) {
+			for (Image i2:list) {
+				if (!list2.contains(i)  && !list2.contains(i2)
+						&& i.getName().equals(i2.getName()) && i.getTime().compareTo(i2.getTime())==0
+						&& !i.getPath().equals(i2.getPath())) {
+					list2.add(i);
+					list2.add(i2);
+					break;
+				}
+			}
+		}
+		
+		return list2;
 	}
 	
 	public boolean listRepeatRemove(String[] id){
-		return imageDao.listRepeatRemove(id);
+		if (id == null || id.length<1) {
+			return false;
+		}
+		String ids = "";
+		for (int i=0;i<id.length;i++) {
+			if(i==0) {
+				ids = id[i];
+			} else {
+				ids += ","+id[i];
+			}
+		}
+		
+		int i = imageMapper.updateState(ids);
+
+		if(i>0) {
+			return true;
+		}
+		return false;
 	}
 	
 	public Set<String> listTime() {
-		List<Date> list = imageDao.listTime();
+		List<Date> list = imageMapper.listTime();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Set<String> dates = new TreeSet<String>(new Comparator<String>() {
 			public int compare(String o1, String o2) {
@@ -105,16 +145,13 @@ public class ImageServiceImpl extends FileServiceImpl implements ImageService{
 	}
 	
 	public List<Image> list(String date) {
-		Date start = null, end = null;
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		try {
-			start = format.parse(date+" 00:00:00");
-			end = format.parse(date+" 23:59:59");
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		String start = date+" 00:00:00";
+		String end = date+" 23:59:59";
+		if (date.length()==7) {
+			start = date+"-01 00:00:00";
+			end = date+"-31 23:59:59";
 		}
-		return imageDao.list(start, end);
+		return imageMapper.getImagesByTimes(start, end);
 	}
 	
 	public Collection<File> scan(String path){
@@ -127,90 +164,46 @@ public class ImageServiceImpl extends FileServiceImpl implements ImageService{
 	
 	public Collection<File> scan(File directory){
 		
-//		File directory = new File(path);
 		String[] extensions = new String[] {"jpg", "jpeg", "JPG", "png", "gif", "GIF"};
 		Collection<File> listFiles = FileUtils.listFiles(directory, extensions, true);
 		
 		return listFiles;
 	}
 	
-	public boolean moveFile(Camera camera, Page page, String orderby) {
-		String[] data = null;
-		
-		int p = 1;
-		do {
-			List<Image> list = imageDao.list(page, orderby, camera);
-			
-			if(p==1) {
-				data = getCachePath(page.getTotal());
-			}
-			
-			if(data!=null) {
-				for (int i=0;i<list.size();i++) {
-					String folder = camera.getPath();
-					if("mobile".equals(camera.getType())) {
-						folder = folder.replace("Cache", "WorkSpace\\Mobile");
-					} else {
-						folder = folder.replace("Cache", "WorkSpace\\Camera");
-					}
-					int j = (page.getPage() - 1) * page.getPageSize() + i;
-					folder = folder + data[j] + "\\";
-					
-					File newFolder = new File(folder);
-					if(!newFolder.exists()){
-						newFolder.mkdir();
-					}
-					
-					Image img = list.get(i);
-					String newPath = folder + img.getName();
-					if(!newPath.equals(img.getPath())) {
-						System.out.println((j+1) + " move file from " + img.getPath() + " to " + newPath);
-						
-						boolean success = new File(img.getPath()).renameTo(new File(newPath));
-						if (success) {
-							System.out.println(" update database " + img.getPath() + " to " + newPath);
-							imageDao.updatePath(newPath, img.getId());
-						}
-					} else {
-						System.out.println((j+1) + " file not move " + img.getId());
-					}
-				}
-				
-			}
-			
-			p = p+1;
-			page.setPage(p);
-		} while (p-1<page.getLast());
-		
-		return false;
-	}
 	
-	public Image getImage(int id) {
-		return imageDao.getImage(id);
-	}
+	
+//	public Image getImage(int id) {
+//		return imageDao.getImage(id);
+//	}
 	public Image getImageByName(String name) {
-		return imageDao.getImageByName(name);
+		return imageMapper.getImageByName(name);
 	}
 	public Image getImage(String name, String path) {
-		return imageDao.getImage(name, path);
+		return imageMapper.getImageByNamePath(name, path);
 	}
 
-	public boolean save(ImageFile file) {
-		return imageDao.save(file);
+//	public boolean save(ImageFile file) {
+//		return imageDao.save(file);
+//	}
+	
+	public List<Map<String, Long>> groupbyCamera() {
+		return imageMapper.groupbyCamera();
 	}
 	
-	public List<Map<String, String>> groupbyCamera() {
-		return imageDao.groupbyCamera();
+	public List<Map<String, Long>> groupbyCamera2() {
+		return imageMapper.groupbyCamera2();
 	}
 	
-	public List<Map<String, String>> groupbyCamera2() {
-		return imageDao.groupbyCamera2();
-	}
-	
-	public Collection<File> findFileNotInDB(Collection<File> files) {
+	/*
+	 * @Deprecated
+	 * 
+	 * 使用的是每次从文件目录拿100个去数据库查有没有，把没有的记录到返回变量。有个缺点，查数据库需要用文件的path，双条件查询，还不如反过来比较
+	 * 
+	 */
+	public Collection<File> findFileNotInDB(Collection<File> files, String dir) {
 		Collection<File> notInDb = new ArrayList<File>();
 		
-		int size = files.size();
+		int size = files.size();System.out.println("files.size:"+size);
 		int circle = 1;
 		int limit = 100;
 		if (size>limit) {
@@ -227,6 +220,7 @@ public class ImageServiceImpl extends FileServiceImpl implements ImageService{
 				p[i] = size;
 			}
 		}
+		// 把302个文件，分3次查询，第一次100个，第二次100个，第三次2个。
 		for (int i = 0; i < p.length; i++) {
 			System.out.println(p[i]);
 		}
@@ -234,39 +228,87 @@ public class ImageServiceImpl extends FileServiceImpl implements ImageService{
 		int d = 0;
 		int i = 0;
 		int j = 0;
-		String[] paths = new String[p[0]];
+		String[] paths = new String[p[0]];//初始化，注意，这里改为存放md5
+		String[] names = new String[p[0]];
 		File[] fs = new File[p[0]];
 		for (File f:files) {
-			
 			if (i==p[d]) {
 				d = d+1;
 				int n = p[d]-(limit*d);
 				paths = new String[n];
+				names = new String[n];
 				fs = new File[n];
 				j = 0;
 			}
-			paths[j] = f.getPath();
+			paths[j] = getImagePath(dir, f);//getFileSavePath(f); DigestUtils.md5Hex(IOUtils.toByteArray(new FileInputStream(file)));
+			names[j] = f.getName();
 			fs[j] = f;
 			
-			if ((i+1)==p[d]) {
-				List<Image> inDB = imageDao.list(paths);
-				System.out.println(inDB.size());
-				kkk(notInDb, fs, inDB);
+			if ((i+1)==p[d]) {// 看上面的描述，换个说法，第100个查询，第200个查询，第300个查询，第302个查询
+				List<Map<String, String>> params = getListParams(paths, names);
+				List<Image> inDB = imageMapper.queryImagesByFullpaths(params);
+				System.out.println("inDB.size:"+inDB.size());
+				setNotInDb(notInDb, fs, inDB, paths);
 			}
 			
 			i++;
 			j++;
 		}
+		System.out.println("notInDb.size:"+notInDb.size());
 		return notInDb;
 	}
+	
+	private List<Map<String, String>> getListParams(String[] paths, String[] names){
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		for (int i=0;i<paths.length;i++) {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("path", paths[i]);
+			map.put("name", names[i]);
+			list.add(map);
+		}
+		return list;
+	}
 
+	public String getImagePath(String dir, File file) {
+		String file_path = file.getPath();
+		String file_name = file.getName();
+		String path = file_path;
+		
+		path = path.replace(dir, "");
+		
+//		String base_path = fileConfig.getBasePath();
+//		if (StringUtils.isNotEmpty(path)) {
+//			path = path.replace(base_path, "");
+//		}
+		
+//		if (bucket!=null 
+//				&& StringUtils.isNotEmpty(bucket.getPath())) {
+//			String bucket_path = bucket.getPath();
+//			path = path.replace(bucket_path, "");
+//		}
+		
+//		if (camer!=null 
+//				&& StringUtils.isNotEmpty(camer.getPath())) {
+//			String camera_path = camer.getPath();
+//			path = path.replace(camera_path, "");
+//		}
+		
+		String name = "\\"+file_name;
+		path = path.replace(name, "");
+		
+		return path;
+	}
+	
 	//把数据库中不存在的保留，比较files和images，把
-	private void kkk(Collection<File> notInDb, File[] files, List<Image> images) {
+	private void setNotInDb(Collection<File> notInDb, File[] files, List<Image> images, String[] paths) {
+		int i = 0;
 		for(File f:files) {
 			boolean notin = true;
+			String path = paths[i];
+			logger.info(path + "|"+f.getName());
 			for(Image img:images) {
-				if (f.getPath().equals(img.getPath())) {
-					notin = false;
+				if (img.getName().equals(f.getName()) && img.getPath().equals(path)) {
+					notin = false;//双否定，就是in
 					break;
 				}
 			}
@@ -274,6 +316,132 @@ public class ImageServiceImpl extends FileServiceImpl implements ImageService{
 				logger.info("pre insert into db:"+f.getPath());
 				notInDb.add(f);
 			}
+			i++;
 		}
 	}
+	
+	public boolean moveFiles(Camera camera, Page page, String orderby) {
+		StringBuffer sb = new StringBuffer();
+		// 初始化
+		int p = 1;//page.getPage();
+		int pageSize = 100;
+		page.setPageSize(pageSize);
+		do {
+			Integer total = imageMapper.getCount(camera);
+			page.setTotal(total);
+			
+			List<Image> list = imageMapper.getImages(page, camera, orderby);//注意，image没有查询关联对象
+			
+			System.out.println("page:"+p+",totalPate:"+page.getLast()+",pageSize:"+pageSize + ",size:" + list.size());
+			moveFiles(list, camera, sb);
+			
+			p = p+1;
+			page.setPage(p);
+		} while (p-1<page.getLast());
+		
+		try {
+			FileUtils.writeStringToFile(new File("D:\\Sya\\Pictures\\test2.txt"), sb.toString(), "GB2312");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private void moveFiles (List<Image> list, Camera camera, StringBuffer sb) {
+		for (int i=0;i<list.size();i++) {
+			moveFile(camera, list.get(i), sb);
+		}
+	}
+	
+	// move source to target
+	private void moveFile(Camera camera, Image img, StringBuffer sb) {
+		String basePath = getBasePath(camera, img);//fileConfig.getBasePath()
+		String newPath = getNewPath(camera, img);
+		//例如 D:\Sya\Pictures\WorkSpace\Camera\LX5\2024\2024-02\2024-02-04
+		String targetPath = fileConfig.getBasePath() + basePath + newPath;
+		
+		File newFolder = new File(targetPath);
+		if(!newFolder.exists()){
+			System.out.println("newPath: not exist, create "+targetPath);
+			newFolder.mkdirs();
+		}
+		
+		String source = fileConfig.getBasePath() + basePath + img.getPath() +"\\"+ img.getName();
+		String target = targetPath +"\\"+ img.getName();
+		if(!newPath.equals(img.getPath())) {//不同于数据库中旧地址才挪动
+			File newFile = new File(target);
+			if (newFile.exists()) {
+				String name = img.getName();
+				if (name.contains(".")) {
+					int d = name.indexOf(".");
+					name = name.substring(0,d) + ".copy" + name.substring(d);
+				} else {
+					name = img.getName() + ".copy";
+				}
+				newFile = new File(targetPath +"\\"+ name);
+			}
+			
+			System.out.println(" move file from [" + source + "] to [" + target+"]\r\n");
+			sb.append("move [").append(source).append("] [").append(target).append("]\r\n");
+			
+			
+//				boolean success = new File(workspace + img.getPath()).renameTo(new File(workspace + newPath));
+			boolean success = new File(source).renameTo(newFile);
+			if (success) {
+				System.out.println(" update database " + img.getPath() + " to " + newPath);
+				imageMapper.updatePath(newPath, img.getId());
+			} else {
+				System.out.println(" move file from " + img.getPath() + " to " + newPath + " fail");
+			}
+		} else {
+			System.out.println(" file not move " + img.getId());
+		}
+	}
+	
+	/**
+	 * 返回相对地址，例如\WorkSpace\Camera\LX5
+	 */
+	private String getBasePath(Camera camera, Image img) {
+		String bucket_path = img.getBucket().getPath();// "\WorkSpace"
+		String camera_path = camera.getPath();// "\Camera\LX5"
+		return bucket_path + camera_path;
+	}
+	
+	/**
+	 * 返回相机目录后的日期地址，例如\2024\2024-01\2024-01-15
+	 * @param camera
+	 * @param img
+	 * @return
+	 */
+	private String getNewPath(Camera camera, Image img) {
+		String date_path = getTime(img.getTime(), camera.getPath());// "\2024\2024-01\2024-01-15"
+		return date_path;
+	}
+	
+	public String getTime(Date date, String type) {
+		SimpleDateFormat sdf = null;
+		sdf = new SimpleDateFormat("\\yyyy\\yyyy-MM\\yyyy-MM-dd");
+		return sdf.format(date);
+	}
+	
+//	public String getFileSavePath(File file) {
+//		String path = file.getPath();
+//		
+//		String workspace = fileConfig.getWorkSpace();
+//		if (workspace.endsWith("\\")) {
+//			workspace = workspace.substring(0, workspace.length()-1);
+//		}
+//		
+//		if (StringUtils.isNotEmpty(path)) {
+//			path = path.replace(workspace, "");
+//		}
+//		return path;
+//	}
+	
+//	public String getCacheSavePath(Image image, int i, Camera camera) {
+//		String workspace = fileConfig.getWorkSpace();
+//		//"D:\Sya\Pictures\WorkSpace\" + "Camera\5D3" + "\100\" + "K28A0019.JPG"
+//		return workspace + camera.getPath() + "\\"+i+"\\" + image.getName();
+//	}
 }
